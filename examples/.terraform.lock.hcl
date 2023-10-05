e74cbcafa7944cb01f57df029afd75acdefeb48f
oleksandr.lytvyn <oleksandr.lytvyn@openvpn.net>
2023-10-05 09:17:51 +0300

    Modify examples

--- examples/.terraform.lock.hcl
+++ examples/.terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "firehydrant.com/twitch/firehydrant" {
   version     = "0.1.0"
   constraints = "0.1.0"
   hashes = [
-    "h1:19+Sc63Aj6pYXUoNyso6zpuloHE0juIwbi+hjyy2WYo=",
+    "h1:dPXyc0TkJd+Jy4vYyLFJKf6X7rCkQ0RFPoCti77Y5z0=",
   ]
 }
--- examples/main.tf
+++ examples/main.tf
@@ -10,23 +10,22 @@ terraform {
 provider "firehydrant" {
 }
 
-# data "firehydrant_service" "monolith" {
-#   id = "d975cf38-922e-4192-b366-dc632e73cd3d"
-# }
+data "firehydrant_service" "qqq" {
+  id = "qqq"
+}
+output "qqq" {
+  value = data.firehydrant_service.qqq.name
+}
 
 # data "firehydrant_service" "lake-service" {
 #   id = "lake-service"
-# }
-
-# output "monolith" {
-#   value = data.firehydrant_service.monolith.name
 # }
 
 # output "lake-service" {
 #   value = data.firehydrant_service.lake-service.name
 # }
 
-# resource "firehydrant_service" "puddles-service" {
-#   name        = "Puddles!!!"
-#   description = "new puddles"
-# }
+resource "firehydrant_service" "puddles-service" {
+  name        = "Puddles!!!"
+  description = "new puddles"
+}
